e23e78c6f727862dbf137a393d21fbcb900d8a93
Mew <ponyashcat228@gmail.com>
2022-07-22 16:12:59 +0300

    toUTCString

--- app/middleware/logger.middleware.mjs
+++ app/middleware/logger.middleware.mjs
@@ -3,7 +3,7 @@ import path from "path";
 export function loggerMiddleware(req, res, next) {
   fs.appendFile(
     path.resolve("logs", "access.log"),
-    `${new Date()} ${req.method} ${req.url} ${req.ip} ${
+    `${new Date().toUTCString()} ${req.method} ${req.url} ${req.ip} ${
       req.headers["user-agent"]
     }\n`,
     (err) => {
