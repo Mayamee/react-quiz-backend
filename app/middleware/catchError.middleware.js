91c93b06f22612c513371b2b6a3762e75649a790
Mayamee <ponyashcat228@gmail.com>
2023-05-15 17:42:33 +0300

    add docker prod

--- app/middleware/catchError.middleware.js
+++ app/middleware/catchError.middleware.js
@@ -5,15 +5,15 @@ export const catchErrorMiddleware = (err, req, res, _next) => {
   if (err instanceof ApiError) {
     const logPattern = `${new Date().toUTCString()} ${err.message} ${err.errors} ${req.method} ${
       req.url
-    } ${req.ip} ${req.headers['user-agent']}\n`
+    } ${req.ip} ${req.headers['user-agent']}`
     console.error(logPattern)
-    writeToErrorLog(logPattern)
+    writeToErrorLog(logPattern + '\n')
     return res.status(err.status).json({ message: err.message, errors: err.errors })
   }
   const logPattern = `${new Date().toUTCString()} ${err.message} ${req.method} ${req.url} ${
     req.ip
-  } ${req.headers['user-agent']}\n`
+  } ${req.headers['user-agent']}`
   console.error(logPattern)
-  writeToErrorLog(logPattern)
+  writeToErrorLog(logPattern + '\n')
   return res.status(501).json({ message: 'Server error' })
 }
